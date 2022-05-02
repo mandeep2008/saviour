f3a2dcf2e9797611342a7c3fb7b528b4c7b940d1
mandeep2008 <mandeepwbk20@gmail.com>
2022-05-02 20:59:08 +0530

    changed title

--- ADMIN/admin_home.php
+++ ADMIN/admin_home.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Online unused medicine donation system.</title>
+    <title>Online unused medicine donation system. Prabh</title>
     <link rel="stylesheet" href="admin_home_style.css">
 </head>
 
