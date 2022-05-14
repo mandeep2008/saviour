a974fd135606ca75e7a57f74cbb318f66e39c975
mandeep2008 <mandeepwbk20@gmail.com>
2022-05-14 18:31:31 +0530

    donated-medicine-table

---
+++ NGO/Donated-medicines.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="Donated-Medicine-style.css">
+</head>
+<body>
+    <h1>Donated Medicines</h1>
+    <table>
+        <tr>
+          <th>ID</th>
+          <th>Donator's Name</th>
+          <th>Donator's Email-id</th>
+          <th>Donator's Address</th>
+          <th>Medicine Brand Name</th>
+          <th>Medicine Generic Name</th>
+          <th>Quantity</th>
+          <th>Expiry Date</th>
+          <th>Medicine Type</th>
+          <th>Donation Type</th>
+          <th>NGO(Your) Email-id</th>
+        </tr>
+        <tr>
+          <td>44</td>
+          <td>abc</td>
+          <td>abc@gmail.com</td>
+          <td>wara bhaika, fraidkot</td>
+          <td>crocin</td>
+          <td>cro123</td>
+          <td>50</td>
+          <td>10-2023</td>
+          <td>Tablet</td>
+          <td>NGO</td>
+          <td>ngo@gmail.com</td>
+        
+        </tr>
+        <tr>
+            <td>44</td>
+            <td>abc</td>
+            <td>abc@gmail.com</td>
+            <td>wara bhaika, fraidkot</td>
+            <td>crocin</td>
+            <td>cro123</td>
+            <td>50</td>
+            <td>10-2023</td>
+            <td>Tablet</td>
+            <td>NGO</td>
+            <td>ngo@gmail.com</td>
+          
+          </tr>
+          <tr>
+            <td>44</td>
+            <td>abc</td>
+            <td>abc@gmail.com</td>
+            <td>wara bhaika, fraidkot</td>
+            <td>crocin</td>
+            <td>cro123</td>
+            <td>50</td>
+            <td>10-2023</td>
+            <td>Tablet</td>
+            <td>NGO</td>
+            <td>ngo@gmail.com</td>
+          
+          </tr>
+      </table>
+      
+</body>
+</html>
